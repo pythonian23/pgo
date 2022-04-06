5b06bcd11ef8c0c656345a184a6026c35d0c8be8
pythonian23 <pythonian23@gmail.com>
2022-04-06 23:47:16 +0900

    Build script

---
+++ scripts/build.fish
@@ -0,0 +1,26 @@
+echo Running from $(pwd)
+
+function build-on -a os arch ver
+    set file \-$ver-$os\_$arch
+    GOOS=$os GOARCH=$arch go build -o=build/pgo$file cmd/pgo
+    echo build/pgo$file
+    GOOS=$os GOARCH=$arch go build -o=build/pgo-discord$file cmd/pgo-discord
+    echo build/pgo-discord$file
+end
+
+gofmt -w .
+goimports -w .
+
+set ver $argv[1]
+set platforms "linux amd64
+linux arm
+linux arm64
+android arm
+android arm64
+windows amd64
+js wasm"
+
+for platform in $(string split \n $platforms)
+    echo $platform
+    build-on $(string split " " $platform) $ver
+end
