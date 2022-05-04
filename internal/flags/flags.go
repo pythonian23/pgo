e7aab21818e6d42f7db4ec13f8bc84bc0b0fd0b2
pythonian23 <pythonian23@gmail.com>
2022-05-04 13:11:27 +0900

    Moved flags to internal pt. 1

---
+++ internal/flags/flags.go
@@ -0,0 +1,24 @@
+package flags
+
+import (
+	"sync"
+
+	flag "github.com/spf13/pflag"
+)
+
+var GeneralFlagSet *flag.FlagSet = flag.NewFlagSet("general", flag.ContinueOnError)
+var IdentityFlagSet *flag.FlagSet = flag.NewFlagSet("identity", flag.ContinueOnError)
+
+var commonArgsLock sync.Mutex = sync.Mutex{}
+var commonArgs Args = Args{
+	generalArgs: &generalArgs{
+		Help: GeneralFlagSet.BoolP("help", "h", false, "Show help about this command."),
+	},
+
+	identityArgs: &identityArgs{
+		Alliance:    IdentityFlagSet.BoolP("alliance", "a", false, "Alliance mode switch. only links and IDs are allowed in alliance mode."),
+		ID:          IdentityFlagSet.IntP("id", "i", 0, "The nation/alliance ID (just the number)"),
+		Link:        IdentityFlagSet.StringP("link", "l", "", "The nation/alliance link."),
+		DiscordName: IdentityFlagSet.StringP("discord", "d", "", "The discord name"),
+	},
+}
