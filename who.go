1910443abd468dc9f05dddedd223834c0f8a27dd
pythonian23 <pythonian23@gmail.com>
2022-05-10 16:06:32 +0900

    Alliance search using ID

--- who.go
+++ who.go
@@ -3,28 +3,47 @@ package pgo
 import (
 	"fmt"
 
+	"github.com/pythonian23/pgo/internal/api"
 	"github.com/pythonian23/pgo/internal/flags"
 )
 
 var whoFlags = flags.NewFlags("who", flags.IdentityFlagSet, flags.APIFlagSet)
 var whoCmd *Command
 
-const whoQuery = "{nations(id:6) {data{nation_name}}}"
+const whoNationQuery = "{nations(%s){data{nation_name}}}"
+const whoAllianceQuery = "{alliances(%s){data{name}}}"
 
-func who(args []string) (string, error) {
+func who(args []string) (out string, err error) {
 	arguments, err := flags.ReadArgs(whoFlags, args)
 	if err != nil {
-		return "", err
+		return
 	}
-	Key = *arguments.APIKey
 	if *arguments.Help {
 		return HelpSubCommand(whoCmd), nil
 	}
-	if *arguments.ID != 0 {
-		data, err := request(whoQuery)
-		return fmt.Sprintln(data.Nations), err
+	Key = *arguments.APIKey
+	var query string
+	if *arguments.Alliance {
+		var alliance *api.Data
+		switch {
+		case *arguments.ID != 0:
+			query = fmt.Sprintf("id:%d", *arguments.ID)
+		default:
+			err = fmt.Errorf("not enough information was provided")
+			return
+		}
+		alliance, err = request(fmt.Sprintf(whoAllianceQuery, query))
+		if err == nil {
+			out = fmt.Sprintln(alliance.Alliances.Data[0].Name)
+		}
+	} else {
+		switch {
+		default:
+			err = fmt.Errorf("not enough information was provided")
+		}
 	}
-	return "not yet implemented", nil
+
+	return
 }
 
 func init() {
