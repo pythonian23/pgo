e1616f409998bc6a6da97162490fe78a6350f10f
pythonian23 <pythonian23@gmail.com>
2022-05-16 02:08:48 +0900

    minimized templates

--- templates.go
+++ templates.go
@@ -2,6 +2,6 @@ package pgo
 
 import "text/template"
 
-var WhoAllianceTemplate = template.Must(template.New("whoAlliance").Parse(`__**{{ .Name }}**__ ({{ .Acronym }})
-*https://politicsandwar.com/alliance/id={{ .ID }}*
-**{{ .Name }}** has a score of *{{ .Score }}*, with *{{ .Members }}* members and *{{ .Applicants }}* applicants. They're involved in a total of *{{ .TotalWars }}* wars. *{{ .OffensiveWars }}* of those are offensive and *{{ .DefensiveWars }}* are defensive. Of those, they are raiding in *{{ .OffensiveRaids }}* wars and are being raided in *{{ .DefensiveRaids }}*. They have a total of *{{ .Resistance }}* resistance and their opponents have a total of *{{ .OpponentResistance }}*.`))
+var WhoAllianceTemplate = template.Must(template.New("whoAlliance").Parse(`__**{{.Name}}**__ ({{.Acronym}})
+*https://politicsandwar.com/alliance/id={{.ID}}*
+**{{.Name}}** has a score of *{{.Score}}*, with *{{.Members}}* members and *{{.Applicants}}* applicants. They're involved in a total of *{{.TotalWars}}* wars. *{{.OffensiveWars}}* of those are offensive and *{{.DefensiveWars}}* are defensive. Of those, they are raiding in *{{.OffensiveRaids}}* wars and are being raided in *{{.DefensiveRaids}}*. They have a total of *{{.Resistance}}* resistance and their opponents have a total of *{{.OpponentResistance}}*.`))
